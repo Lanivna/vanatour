4a441e277f944d7c7898f2f7d24c13b080f3f863
Denys Halenok <denys.halenok@gmail.com>
2019-05-16 09:14:43 +0300

    Implement Navbar and basic pages

---
+++ src/scenes/PlaneTickets/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class PlaneTickets extends React.Component {
+  render() {
+    return <h1>PlaneTickets</h1>
+  }
+}
+
+export default PlaneTickets;
---
+++ src/scenes/Visas/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class Visas extends React.Component {
+  render() {
+    return <h1>Visas</h1>
+  }
+}
+
+export default Visas;
