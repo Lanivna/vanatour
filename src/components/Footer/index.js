b1378737c93cc78b44ff82ed1d9bdc595c923574
Denys Halenok <denys.halenok@gmail.com>
2019-05-16 15:47:55 +0300

    Create footer

---
+++ src/components/Footer/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { FaFacebookF, FaTwitter, FaVk, FaInstagram, FaGoogle } from 'react-icons/fa';
+
+import "./style.scss";
+
+class Footer extends React.Component {
+  render() {
+    return (
+      <footer>
+        <div className="left">
+          <a href="#"><FaFacebookF /></a>
+          <a href="#"><FaTwitter /></a>
+          <a href="#"><FaVk /></a>
+          <a href="#"><FaInstagram /></a>
+          <a href="#"><FaGoogle /></a>
+        </div>
+        <div className="center">
+          <span>© 2016 - 2018 | All rights reserved</span>
+        </div>
+        <div className="right" />
+      </footer>
+    );
+  }
+}
+
+export default Footer;
--- src/scenes/Contact/index.js
+++ src/scenes/Contact/index.js
@@ -1,54 +1,58 @@
-import React from "react";
+import React, {Fragment} from "react";
 import {Button, Card, FormGroup, InputGroup, TextArea} from "@blueprintjs/core";
 import "./style.scss";
 import {Intent} from "@blueprintjs/core/lib/cjs/common/intent";
 import Background from "./components/Background";
+import Footer from "../../components/Footer";
 
 class Contact extends React.Component {
   render() {
     return (
-      <div className="content-wrapper">
-        <Background />
-        <h1 className="bp3-heading">Контакты</h1>
-        <div className="cards">
-          <Card className="custom-card" elevation="3">
-            <form>
-              <FormGroup
-                label="Full Name"
-                labelFor="full-name"
-                labelInfo="(required)"
-              >
-                <InputGroup id="full-name" type="text" required />
-              </FormGroup>
-              <FormGroup
-                  label="E-mail"
-                  labelFor="email"
+      <Fragment>
+        <div className="content-wrapper">
+          <Background />
+          <h1 className="bp3-heading">Контакты</h1>
+          <div className="cards">
+            <Card className="custom-card" elevation="3">
+              <form>
+                <FormGroup
+                  label="Full Name"
+                  labelFor="full-name"
                   labelInfo="(required)"
                 >
-                  <InputGroup id="email" type="email" required />
-              </FormGroup>
-              <FormGroup
-                  label="Message"
-                  labelFor="message"
-                  labelInfo="(required)"
-                >
-                  <TextArea
-                    required
-                    growVertically={false}
-                  />
-              </FormGroup>
-              <div style={{textAlign: "center"}}>
-                <Button icon="envelope" intent={Intent.PRIMARY} type="submit">
-                  Send
-                </Button>
-              </div>
-            </form>
-          </Card>
-          <Card className="custom-card" elevation="3">
+                  <InputGroup id="full-name" type="text" required />
+                </FormGroup>
+                <FormGroup
+                    label="E-mail"
+                    labelFor="email"
+                    labelInfo="(required)"
+                  >
+                    <InputGroup id="email" type="email" required />
+                </FormGroup>
+                <FormGroup
+                    label="Message"
+                    labelFor="message"
+                    labelInfo="(required)"
+                  >
+                    <TextArea
+                      required
+                      growVertically={false}
+                    />
+                </FormGroup>
+                <div style={{textAlign: "center"}}>
+                  <Button icon="envelope" intent={Intent.PRIMARY} type="submit">
+                    Send
+                  </Button>
+                </div>
+              </form>
+            </Card>
+            <Card className="custom-card" elevation="3">
 
-          </Card>
+            </Card>
+          </div>
         </div>
-      </div>
+        <Footer />
+      </Fragment>
     );
 
 
